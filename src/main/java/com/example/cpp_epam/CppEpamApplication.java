28437632e9eade47731e0f758b48c3102693c3d8
Varyasha <varvara.przh@gmail.com>
2022-02-26 20:30:12 +0300

    Initial commit

---
+++ src/main/java/com/example/cpp_epam/CppEpamApplication.java
@@ -0,0 +1,13 @@
+package com.example.cpp_epam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CppEpamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CppEpamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cpp_epam/CppEpamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cpp_epam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CppEpamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
